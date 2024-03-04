2fe5928ffdb30bdaf67f7618c55df766540fbe37
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2024-03-04 09:57:37 +0100

    Added NuGet license, NuGet Readme and build script

---
+++ build.ps1
@@ -0,0 +1,11 @@
+& dotnet tool restore
+
+& dotnet restore ./src/Mammoth.Extensions.DependencyInjection.sln
+
+& dotnet tool run dotnet-gitversion /updateprojectfiles
+
+& dotnet build ./src/Mammoth.Extensions.DependencyInjection.sln --configuration Release --no-restore
+
+# & dotnet test ./src/Mammoth.Extensions.DependencyInjection.sln --configuration Release --no-build --verbosity normal
+
+& dotnet pack ./src/Mammoth.Extensions.DependencyInjection.sln --configuration Release --no-build --output ./artifacts --include-symbols
